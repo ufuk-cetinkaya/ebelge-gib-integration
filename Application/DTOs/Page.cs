bf94628a683acbae12a2b9c8bf83d0095f4b1777
Ufuk Cetinkaya <ufuk.cetinkya@gmail.com>
2026-04-08 09:41:02 +0300

    unused methods removed

--- Application/DTOs/Page.cs
+++ Application/DTOs/Page.cs
@@ -6,75 +6,21 @@ public class Page<T>
     private readonly int _totalRecords;
     private readonly int _pageSize;
     private readonly int _pageCount;
-    private bool _hasPreviousPage;
-    private bool _hasNextPage;
-    private bool _isFirstPage;
-    private bool _isLastPage;
-    private int _skip;
-    private int _fetch;
-    private int _currentPage;
-    public IEnumerable<T>? _data;
+    private readonly bool _hasPreviousPage;
+    private readonly bool _hasNextPage;
+    private readonly bool _isFirstPage;
+    private readonly bool _isLastPage;
+    private readonly int _skip;
+    private readonly int _fetch;
+    private readonly int _currentPage;
 
     public Page(int totalRecords, int pageSize, int currentPage)
     {
-        // Sayfa sayısının negatif olmaması için
         _totalRecords = Math.Max(totalRecords, 0);
-        // Sıfıra bölme hatası almaması ve sayfa sayısının negatif olmaması için
         _pageSize = Math.Max(pageSize, 1);
         _pageCount = Convert.ToInt32(Math.Ceiling(decimal.Divide(_totalRecords, _pageSize)));
         _currentPage = currentPage;
-        SetParams();
-    }
 
-    public int TotalRecords => _totalRecords;
-    public int PageCount => _pageCount;
-    public bool HasPreviousPage => _hasPreviousPage;
-    public bool HasNextPage => _hasNextPage;
-    public bool IsFirstPage => _isFirstPage;
-    public bool IsLastPage => _isLastPage;
-    public int Skip => _skip;
-    public int Fetch => _fetch;
-
-    public IEnumerable<T>? Data { get; set; }
-
-    public void First()
-    {
-        if (!_isFirstPage)
-        {
-            _currentPage = 1;
-            SetParams();
-        }
-    }
-
-    public void Last()
-    {
-        if (!_isLastPage)
-        {
-            _currentPage = _pageCount;
-            SetParams();
-        }
-    }
-
-    public void Previous()
-    {
-        if (_hasPreviousPage)
-        {
-            _currentPage--;
-            SetParams();
-        }
-    }
-
-    public void Next()
-    {
-        if (_hasNextPage)
-        {
-            _currentPage++;
-            SetParams();
-        }
-    }
-
-    private void SetParams()
-    {
         if (_pageCount == 0)
         {
             _hasPreviousPage = false;
@@ -96,4 +42,14 @@ public class Page<T>
 
         _fetch = Math.Max((_currentPage * _pageSize) - _skip, 0);
     }
+
+    public int TotalRecords => _totalRecords;
+    public int PageCount => _pageCount;
+    public bool HasPreviousPage => _hasPreviousPage;
+    public bool HasNextPage => _hasNextPage;
+    public bool IsFirstPage => _isFirstPage;
+    public bool IsLastPage => _isLastPage;
+    internal int Skip => _skip;
+    internal int Fetch => _fetch;
+    public IEnumerable<T>? Data { get; set; }
 }
